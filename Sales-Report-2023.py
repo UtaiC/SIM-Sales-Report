79217f8e84251b0b7823a8afb0f151f41090803f
Utai <74049768+UtaiC@users.noreply.github.com>
2023-04-20 18:14:03 +0700

    Update Sales-Report-2023.py

--- Sales-Report-2023.py
+++ Sales-Report-2023.py
@@ -18,7 +18,6 @@ from dateutil import parser
 Logo=Image.open('SIM-LOGO-02.jpg')
 st.image(Logo,width=700)
 st.markdown("<h2 style='text-align: center; color:#F1F0E5'>Sales Report 2023 </h2>",unsafe_allow_html=True)
-# st.subheader("Sales 2023 Report:")
 ##################
 db=pd.read_excel('Database-2022.xlsx')
 #################
@@ -30,50 +29,50 @@ def load_data_from_drive():
 data = load_data_from_drive()
 Invoices=data
 
-# url="https://docs.google.com/spreadsheets/d/1sUH0WmtfrWbR8FrM33ljebSlW0BWE3w0/export?format=xlsx"
-# Invoices=pd.read_excel(url,header=4)
 Invoices[['วันที่','เลขที่','ลูกค้า','ชื่อสินค้า']]=Invoices[['วันที่','เลขที่','ลูกค้า','ชื่อสินค้า']].astype(str)
 Invoices=Invoices[(~Invoices['รหัสสินค้า'].astype(str).str.contains('MOLD-T0'))]
 Invoices=Invoices[(~Invoices['รหัสสินค้า'].astype(str).str.contains('MOLD-T1'))]
 Inv=Invoices[['วันที่','เลขที่','ลูกค้า','ชื่อสินค้า','จำนวน','มูลค่าสินค้า','รหัสสินค้า']]
 Inv=Invoices[Invoices['เลขที่'].str.contains('IV')|Invoices['เลขที่'].str.contains('HS')]
-# TTCN=Invoices[Invoices['เลขที่'].str.contains('SR')]
-# TTCN[['ลูกค้า','มูลค่าสินค้า']]
-# TTDN=Invoices[Invoices['รหัสสินค้า'].str.contains('DR')]
-# TTDN[['ลูกค้า','มูลค่าสินค้า']]
+
 ###################### Total Invoice Check #################################################
 Inv['มูลค่าสินค้า']= pd.to_numeric(Inv['มูลค่าสินค้า'], errors='coerce')
 Inv['มูลค่าสินค้า'].dropna(0, inplace=True)
 TTSales=Inv['มูลค่าสินค้า']
 TTSales=TTSales.sum()
-################ Select ############################
-MonthSelected=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
-Minput=st.selectbox('MonthSelected',MonthSelected, key='unique-key-1')
-MAPYM={'Month':['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']}
-YMAP={'Jan':'2023-01-01','Feb':'2023-02-01','Mar':'2023-03-01','Apr':'2023-04-01','May':'2023-05-01','Jun':'2023-06-01','Jul':'2023-07-01',
-'Aug':'2023-08-01','Sep':'2023-09-01','Oct':'2023-10-01','Nov':'2023-11-01','Dec':'2023-12-31-01'}
-MAPYM=pd.DataFrame(MAPYM)
-MAPYM['Year']=MAPYM['Month'].map(YMAP)
-MAPYM=MAPYM[MAPYM['Month']==Minput]
-Y=MAPYM['Year'].to_string(index=False)
-YMInput=Y
-Y
-################ Range ############################
-Minput2=st.selectbox('Range of MonthSelected',MonthSelected, key='unique-key-2')
-MAPYM2={'Month':['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']}
-YMAP2={'Jan':'2023-01-31','Feb':'2023-02-28','Mar':'2023-03-31','Apr':'2023-04-30','May':'2023-05-31','Jun':'2023-06-30','Jul':'2023-07-31',
-'Aug':'2023-08-31','Sep':'2023-09-30','Oct':'2023-10-31','Nov':'2023-11-30','Dec':'2023-12-31'}
-for key, value in YMAP2.items():
-    YMAP2[key] = pd.to_datetime(value) + pd.Timedelta(days=1)
-MAPYM2=pd.DataFrame(MAPYM2)
-MAPYM2['Year']=MAPYM2['Month'].map(YMAP2)
-MAPYM2=MAPYM2[MAPYM2['Month']==Minput2]
-Y2=MAPYM2['Year'].to_string(index=False)
-YMInput2=Y2
-Y2
+Minput = st.selectbox('Select month', ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], key='unique-key-1')
+
+map_ym = {'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}
+y_map = {'Jan': '2023-01-01', 'Feb': '2023-02-01', 'Mar': '2023-03-01', 'Apr': '2023-04-01', 'May': '2023-05-01', 'Jun': '2023-06-01', 'Jul': '2023-07-01',
+         'Aug': '2023-08-01', 'Sep': '2023-09-01', 'Oct': '2023-10-01', 'Nov': '2023-11-01', 'Dec': '2023-12-01'}
+
+map_ym = pd.DataFrame(map_ym)
+map_ym['Year'] = map_ym['Month'].map(y_map)
+map_ym = map_ym[map_ym['Month'] == Minput]
+
+y = map_ym['Year'].to_string(index=False)
+ym_input = y.strip()
+ym_input
+# Range
+Minput2 = st.selectbox('Select range of months', ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], key='unique-key-2')
+
+map_ym_range = {'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}
+y_map_range = {'Jan': '2023-01-31', 'Feb': '2023-02-28', 'Mar': '2023-03-31', 'Apr': '2023-04-30', 'May': '2023-05-31', 'Jun': '2023-06-30', 'Jul': '2023-07-31',
+               'Aug': '2023-08-31', 'Sep': '2023-09-30', 'Oct': '2023-10-31', 'Nov': '2023-11-30', 'Dec': '2023-12-31'}
+
+for key, value in y_map_range.items():
+    y_map_range[key] = pd.to_datetime(value) + pd.Timedelta(days=1)
+
+map_ym_range = pd.DataFrame(map_ym_range)
+map_ym_range['Year'] = map_ym_range['Month'].map(y_map_range)
+map_ym_range = map_ym_range[map_ym_range['Month'] == Minput2]
+
+y2 = map_ym_range['Year'].to_string(index=False)
+ym_input2 = y2.strip()
+ym_input2
 #########################################################
-ym_Count = Y[:7]
+ym_Count = ym_input[:7]
 DayCount=Invoices['วันที่']
 DayCount=DayCount[DayCount.str.contains(ym_Count)]
 DayCount=DayCount.drop_duplicates()
@@ -90,24 +89,24 @@ st.write('Invoice Isuued Days:',COUNT)
 ###################### MASS ################################
 TotalMASS=Invoices[Invoices['ลูกค้า'].str.contains('VALEO')|Invoices['ลูกค้า'].str.contains('แครทโค')|Invoices['ชื่อสินค้า'].str.contains('PACKING')|
 Invoices['ลูกค้า'].str.contains('เซนทรัล')]
-TotalMASS=TotalMASS[TotalMASS['วันที่'].between( YMInput, YMInput2)]
+TotalMASS=TotalMASS[TotalMASS['วันที่'].between( ym_input, ym_input2)]
 TotalMASS=pd.merge(TotalMASS,db[['Part_No','Mold-PM','Mold-DP']],left_on='รหัสสินค้า',right_on='Part_No',how='left')
 ####################### Steel Bush ################################
 TotalSTB=Invoices[Invoices['ชื่อสินค้า'].str.contains('STEEL')]
-TotalSTB = TotalSTB[TotalSTB['วันที่'].between( YMInput, YMInput2)]
+TotalSTB = TotalSTB[TotalSTB['วันที่'].between( ym_input, ym_input2)]
 ######################### Mold #########################################
 TotalMOLD=Invoices[Invoices['รหัสสินค้า'].astype(str).str.contains('MOLD')|
 Invoices['รหัสสินค้า'].astype(str).str.contains('PART')|
 Invoices['รหัสสินค้า'].astype(str).str.contains('REPAIR')]
-TotalMOLD=TotalMOLD[TotalMOLD['วันที่'].between( YMInput, YMInput2)]
+TotalMOLD=TotalMOLD[TotalMOLD['วันที่'].between( ym_input, ym_input2)]
 ######################## Other ###############################
 TotalOTHER=Invoices[Invoices['ชื่อสินค้า'].str.contains('DENSE')|Invoices['ชื่อสินค้า'].str.contains('RTV')|Invoices['ชื่อสินค้า'].str.contains('ตู้')]
-TotalOTHER=TotalOTHER[TotalOTHER['วันที่'].between( YMInput, YMInput2)]
+TotalOTHER=TotalOTHER[TotalOTHER['วันที่'].between( ym_input, ym_input2)]
 ####################### Cash ##################################
-SalesCash=SalesCash[SalesCash['วันที่'].between( YMInput, YMInput2)]
+SalesCash=SalesCash[SalesCash['วันที่'].between( ym_input, ym_input2)]
 SalesCash=SalesCash[['วันที่','มูลค่าสินค้า']]
 ############################################################
-ONESIM=Inv[Inv['วันที่'].between( YMInput, YMInput2)]
+ONESIM=Inv[Inv['วันที่'].between( ym_input, ym_input2)]
 ONESIM=ONESIM.groupby('ลูกค้า').agg({'มูลค่าสินค้า':'sum','ชื่อสินค้า':'first'})
 ONESIM
 #########################################################
@@ -135,8 +134,8 @@ SUMSALES.set_index('Items',inplace=True)
 from pandas.tseries.offsets import BDay
 
 # specify the start and end dates for the date range
-start_date = Y
-end_date = Y2
+start_date = ym_input
+end_date = ym_input2
 
 # create a pandas date range for the specified date range
 date_range = pd.date_range(start=start_date, end=end_date)
@@ -154,7 +153,13 @@ Target2023=list(Target2023)
 st.write('---')
 st.write('**Credit Note Details-MASS**')
 TotalMASSCN = TotalMASS[TotalMASS['เลขที่'].str.contains('SR')]
-TotalMASSCN[['ลูกค้า', 'มูลค่าสินค้า']]
+
+if TotalMASSCN.empty:
+    st.write("No MASS Credit Note.")
+else:
+    st.dataframe(TotalMASSCN[['ลูกค้า', 'มูลค่าสินค้า']])
+
+# TotalMASSCN[['ลูกค้า', 'มูลค่าสินค้า']]
 TotalMASSCN = TotalMASSCN[['ลูกค้า', 'มูลค่าสินค้า']].groupby('ลูกค้า').sum()
 MASSCN = 0  # initialize MASSCN to 0
 try:
@@ -165,7 +170,11 @@ st.write('Total Credit Note Details-MASS:', MASSCN)
 st.write('---')
 st.write('**Credit Note Details-MOLD**')
 TotalMoldCN = TotalMOLD[TotalMOLD['เลขที่'].str.contains('SR')]
-TotalMoldCN[['ลูกค้า', 'มูลค่าสินค้า']]
+if TotalMoldCN.empty:
+    st.write('No Mold Credit Note')
+else:
+    st.dataframe(TotalMoldCN[['ลูกค้า', 'มูลค่าสินค้า']])
+
 TotalMoldCN = TotalMoldCN[['ลูกค้า', 'มูลค่าสินค้า']].groupby('ลูกค้า').sum()
 MOLDCN = 0  # initialize MOLDCN to 0
 try:
@@ -177,7 +186,11 @@ st.write('---')
 ##########################################################################
 st.write('**Debit Note Details-MASS**')
 TotalMASSDN = TotalMASS[TotalMASS['เลขที่'].str.contains('DR')]
-TotalMASSDN[['ลูกค้า', 'มูลค่าสินค้า']]
+if TotalMASSDN.empty:
+    st.write('No Mold Debit Note')
+else:
+    st.daraframe(TotalMASSDN[['ลูกค้า', 'มูลค่าสินค้า']])
+
 TotalMASSDN = TotalMASSDN[['ลูกค้า', 'มูลค่าสินค้า']].groupby('ลูกค้า').sum()
 MASSDN = 0  # initialize MASSDN to 0
 try:
@@ -188,7 +201,11 @@ st.write('Total Debit Note Details-MASS:', MASSDN)
 st.write('---')
 st.write('**Debit Note Details-MOLD**')
 TotalMoldDN = TotalMOLD[TotalMOLD['เลขที่'].str.contains('DR')]
-TotalMoldDN[['ลูกค้า', 'มูลค่าสินค้า']]
+if TotalMoldDN.empty:
+    st.write('No Mold Debit Note')
+else:
+    st.daraframe(TotalMoldDN[['ลูกค้า', 'มูลค่าสินค้า']])
+
 TotalMoldDN = TotalMoldDN[['ลูกค้า', 'มูลค่าสินค้า']].groupby('ลูกค้า').sum()
 MOLDDN = 0  # initialize MOLDDN to 0
 try:
@@ -207,7 +224,6 @@ num_months = (end_date.year - start_date.year) * 12 + end_date.month - start_dat
 # Define the data for the bar chart
 categories = ['One-SIM','MASS','Mold','Steel Bush','Cash','Other']
 values = [TotalSales+TotalSaleCASH, TotalSalesMASS+(MASSCN+MASSDN),TotalSalesMOLD+(MOLDCN+MOLDDN),TotalSaleSTB,TotalSaleCASH,(TotalSalesOTHER)]
-# values2 = [((19166666.66/(250/12))*COUNT),((13940823/(250/12))*COUNT),((954000/(250/12))*COUNT),((4350374.17/(250/12))*COUNT),0,0]
 values2 =Target2023
 # Use num_months as the monthly factor to multiply the values in values and values2
 monthly_factor = num_months
@@ -215,10 +231,7 @@ for i in range(len(values2)):
     values2[i] = values2[i] * monthly_factor
 ##########################################################
 today = datetime.datetime.today().strftime('%Y-%m-%d')
-# Create a list of labels with the "k" suffix
-# labels = [f"{value/1000:.0f}k" for value in values]
-# labels = [f"{int(value)}" for value in values]
-# labels = [f"{value:.2f}" for value in values]
+
 labels = [f"{value:,.0f}" for value in values]
 labels2 = [f"{value:,.0f}" for value in values2]
 # Create the bar chart
