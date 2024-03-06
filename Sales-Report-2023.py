2c278d3e6f06e5231cbffded0964527932df0110
Utai <74049768+UtaiC@users.noreply.github.com>
2024-03-06 16:26:47 +0700

    Update Sales-Report-2023.py

--- Sales-Report-2023.py
+++ Sales-Report-2023.py
@@ -3,7 +3,7 @@ from ast import In, Str
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from PIL import Image
 import altair as alt
 from datetime import date
